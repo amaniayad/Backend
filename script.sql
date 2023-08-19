e0ccdd0153ec39aa43a21edc97fd9eab158102f8
amaniayad <a.ayad@esi-sba.dz>
2023-08-19 12:33:52 +0200

    sql script modified

--- script.sql
+++ script.sql
@@ -1,20 +1,58 @@
-CREATE DATABASE riad;
-use riad;
-CREATE TABLE user (
-  id INTEGER PRIMARY KEY,
-  username TEXT NOT NULL,
-  address TEXT NOT NULL,
-   email TEXT NOT NULL,
-  password varchar(255) NOT NULL,
-  role TEXT NOT NULL
-);
+`cartes`SELECT `cartes`.`num`,
+    `cartes`.`dateExperation`,
+    `cartes`.`codeCarte`,
+    `cartes`.`credit`
+FROM `projet1cs`.`cartes`;
 
-CREATE TABLE products (
-  id INTEGER PRIMARY KEY,
-  nom TEXT NOT NULL,
-  prix REAL NOT NULL,
-  marque TEXT NOT NULL,
-  quantite int not null,
-  vendue BOOLEAN,
-  taille TEXT NOT NULL,
-   );
+SELECT `commande`.`id`,
+    `commande`.`id_user`,
+    `commande`.`date_achat`,
+    `commande`.`paymentOnline`,
+    `commande`.`numCarte`,
+    `commande`.`fullname`,
+    `commande`.`fullAddress`,
+    `commande`.`numTel`,
+    `commande`.`total`,
+    `commande`.`état`
+FROM `projet1cs`.`commande`;
+
+SELECT `panier`.`id`,
+    `panier`.`id_user`,
+    `panier`.`id_produit`,
+    `panier`.`nom_produit`,
+    `panier`.`date_ajout`,
+    `panier`.`quantite`,
+    `panier`.`prix`
+FROM `projet1cs`.`panier`;
+
+SELECT `products`.`id`,
+    `products`.`photo`,
+    `products`.`matricule`,
+    `products`.`nom`,
+    `products`.`prix`,
+    `products`.`marque`,
+    `products`.`categorie`,
+    `products`.`quantite`,
+    `products`.`taille`,
+    `products`.`couleur`
+FROM `projet1cs`.`products`;
+
+SELECT `produit_commande`.`id_commande`,
+    `produit_commande`.`id_produit`,
+    `produit_commande`.`quantite`
+FROM `projet1cs`.`produit_commande`;
+
+SELECT `user`.`id`,
+    `user`.`username`,
+    `user`.`address`,
+    `user`.`email`,
+    `user`.`password`,
+    `user`.`role`,
+    `user`.`activeAccount`
+FROM `projet1cs`.`user`;
+
+SELECT `wishlist`.`id`,
+    `wishlist`.`id_produit`,
+    `wishlist`.`id_user`
+FROM `projet1cs`.`wishlist`;
+
